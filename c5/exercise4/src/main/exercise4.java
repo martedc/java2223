9c697b83fe88d3559b1ac67c4f5d22cfecb9f2a1
martedc <marten.de.cock@gmail.com>
2022-10-22 09:41:35 +0200

    c5 exercises

--- c5/exercise4/src/main/exercise4.java
+++ c5/exercise4/src/main/exercise4.java
@@ -1,6 +1,40 @@
 package main;
 
+import java.util.ArrayList;
+
 public class exercise4 {
-	
-	
+
+	public static ArrayList<Integer> getFirstPrimesUsingForLoops(int nbPrimes) {
+
+		ArrayList<Integer> primeNumbers = new ArrayList<>();
+		primeNumbers.add(2);
+		boolean isPrime;
+		int i = 3;
+
+		while (primeNumbers.size() < nbPrimes) {
+			
+			isPrime = true;
+			int j = i - 1;
+			
+			while (j >= 2) {
+				if (i % j == 0) {
+					isPrime = false;
+				}
+				j--;
+
+			}
+			if (isPrime) {
+				primeNumbers.add(i);
+			}
+			i++;
+		}
+
+		return primeNumbers;
+	}
+
+	public static void main(String[] args) {
+		
+		System.out.println(getFirstPrimesUsingForLoops(10));
+	}
+
 }
