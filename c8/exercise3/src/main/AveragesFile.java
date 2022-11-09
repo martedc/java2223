64ffc625afbe8420e32778b000249da7fcb48e49
martedc <marten.de.cock@gmail.com>
2022-11-09 11:32:12 +0100

    c8 exercises

--- c8/exercise3/src/main/AveragesFile.java
+++ c8/exercise3/src/main/AveragesFile.java
@@ -1,9 +1,7 @@
 package main;
 
 import java.util.*;
-import java.io.FileNotFoundException;
-import java.io.File;
-import java.io.BufferedWriter;
+import java.io.*;
 
 public class AveragesFile {
 
@@ -22,26 +20,47 @@ public class AveragesFile {
 		Scanner scanFile = new Scanner(System.in);
 		double num = 1;
 		String inputFile;
-		String outputFile;
-
-		// ask user for inputs
+		FileWriter fw = null;
+		BufferedWriter bw = null;
+		PrintWriter pw = null;
+	
 		try {
+			// ask user for inputs
 			System.out.println("Please enter the filename: (with extension)");
 			inputFile = "src/" + scanFile.nextLine();
 			File input = new File(inputFile);
 			scanFile.close();
 			Scanner scan = new Scanner(input);
-			
 			do {
 				num= scan.nextDouble();
 				numList.add(num);
 			} while (scan.hasNextDouble());
 			scan.close();
-		} catch (Exception e) {
-			System.out.println("An error has occured.");
-		}
+			
+			// add average to file
+			fw = new FileWriter(inputFile, true);
+			bw = new BufferedWriter(fw);
+			pw = new PrintWriter(bw);
+			
+			pw.println("Average:");
+			pw.println(calculateAverage(numList));
+			
+		} catch (FileNotFoundException e) {
+			System.out.println("File was not found.");
+		} catch (IOException e1) {
+			System.out.println("IO error.");
+		} finally {
+			try {
+				pw.close();
+				bw.close();
+				fw.close();
+			} catch (IOException e2) {
+				System.out.println("IO error.");
+			}
+			
+		} 
 		
-		// add average to file
+		
 
 		// calculate and print average
 		System.out.println(numList);
