732e249fa3c390a2d0981e60c71005c6940434bd
martedc <marten.de.cock@gmail.com>
2022-12-30 10:44:19 +0100

    final changes in main method

--- project/alignments/src/main/Main.java
+++ project/alignments/src/main/Main.java
@@ -60,30 +60,54 @@ public class Main {
 		 * All following code are the employees executing some of their methods to make changes to alignments, write to files, etc.
 		 */
 		
-//		employeeList.printEmployees();
-//		employeeList.getBioinformatician(0).findSequence(false, "ACACAGAGCTAAGGCAGTAGCAGATTTCAAAA");
-//		employeeList.getBioinformatician(0).replaceSequence(">1997.D.KE.97.ML415", "ACACAGAGCTAAGGCAGTAGCAGATTTCAAAA", "ACACAGAACTAGGGCAGTAACAGATTCCAAAA", repo);
-//		employeeList.getTeamLead(0).setOptimalAlignment(employeeList.getBioinformatician(0), repo);
-//		employeeList.getBioinformatician(1).replaceSequence("ACATTT", "ACGTCT", repo);
-//		employeeList.getTeamLead(0).setOptimalAlignment(employeeList.getBioinformatician(1), repo);
-//		employeeList.getTeamLead(0).setAlignment(employeeList.getBioinformatician(2), repo);
-//		employeeList.getTechSupport(0).setBackup(repo);
-//		employeeList.getTeamLead(0).writeReportToFile(repo);
-//		employeeList.getBioinformatician(2).replaceSequence(">2002.F1.AR.02.ARE933", "AGGAGAGCC", "AGGGGGGCC", repo);
-//		employeeList.getTechSupport(0).restoreRepository(repo);
-//		employeeList.getBioinformatician(2).writeReportToFile();
-//		employeeList.getTeamLead(0).setOptimalAlignment(employeeList.getBioinformatician(2), repo);
-//		employeeList.getBioinformatician(0).removeGenome(">1997.D.KE.97.ML415", repo);
-		
+		// Start with printing an overview of all employees
 		employeeList.printEmployees();
-		employeeList.getBioinformatician(1).replaceSequence("TGTCC", "TTTCC", repo);
-		employeeList.getBioinformatician(2).replaceSequence(">2001.F1.BR.01.01BR087", "AAGTTACCG", "AAATTAAGG", repo);
-		employeeList.getTeamLead(0).setOptimalAlignment(employeeList.getBioinformatician(2), repo);
-		employeeList.getTeamLead(0).setAlignment(employeeList.getBioinformatician(1), repo);
+		
+		// The first bioinformatician in the list finds all genomes with sequence "ACACAGAGCTAAGGCAGTAGCAGATTTCAAAA"
+		employeeList.getBioinformatician(0).findSequence(false, "ACACAGAGCTAAGGCAGTAGCAGATTTCAAAA");
+		
+		// Bioinformatician 1 replaces sequence "ACACAGAGCTAAGGCAGTAGCAGATTTCAAAA" with "ACACAGAACTAGGGCAGTAACAGATTCCAAAA" in genome ">1997.D.KE.97.ML415"
+		employeeList.getBioinformatician(0).replaceSequence(">1997.D.KE.97.ML415", "ACACAGAGCTAAGGCAGTAGCAGATTTCAAAA", "ACACAGAACTAGGGCAGTAACAGATTCCAAAA", repo);
+		
+		// The first team leader sets bioinformatician 1's alignment to the optimal alignment in the repository
+		employeeList.getTeamLead(0).setOptimalAlignment(employeeList.getBioinformatician(0), repo);
+		
+		// Bioinformatician 2 replaces all sequences "ACATTT" with "ACGTCT" in his alignment
+		employeeList.getBioinformatician(1).replaceSequence("ACATTT", "ACGTCT", repo);
+		
+		// Team leader 1 sets bioinformatician 2's alignment to the optimal alignment in the repository
+		employeeList.getTeamLead(0).setOptimalAlignment(employeeList.getBioinformatician(1), repo);
+		
+		// Team leader 1 replaces bioinformatician 3's alignment with optimal alignment from the repository
+		employeeList.getTeamLead(0).setAlignment(employeeList.getBioinformatician(2), repo);
+		
+		// Tech support 1 takes a backup of the current repository
 		employeeList.getTechSupport(0).setBackup(repo);
-		employeeList.getBioinformatician(1).replaceSequence("ACGC","TATC", repo);
+		
+		// Team lead 1 writes a report of all bioinformatician's scores to file
+		employeeList.getTeamLead(0).writeReportToFile(repo);
+		
+		// Bioinformatician 3 replaces "AGGAGAGCC" with "AGGGGGGCC" in genome ">2002.F1.AR.02.ARE933"
+		employeeList.getBioinformatician(2).replaceSequence(">2002.F1.AR.02.ARE933", "AGGAGAGCC", "AGGGGGGCC", repo);
+		
+		// Tech support 1 restores the repository from his backup
 		employeeList.getTechSupport(0).restoreRepository(repo, employeeList);
-		System.out.println(employeeList.getBioinformatician(1).getAlignment().getScore());
+		
+		// Bioinformatician 3 writes his alignment's score to file.
+		employeeList.getBioinformatician(2).writeReportToFile();
+		
+		// Team lead 1 sets bioinformatician 3's alignment to optimal alignment in the repository
+		employeeList.getTeamLead(0).setOptimalAlignment(employeeList.getBioinformatician(2), repo);
+		
+		// Bioinformatician 1 removes genome ">1997.D.KE.97.ML415" from his alignment
+		employeeList.getBioinformatician(0).removeGenome(">1997.D.KE.97.ML415", repo);
+		
+		// And finishing off with some writing some files
+		employeeList.getBioinformatician(2).writeAlignmentToFile();
+		employeeList.getBioinformatician(0).writeReportToFile();
+		employeeList.getTeamLead(0).writeAlignmentToFile(repo);
+		employeeList.getBioinformatician(1).writeReportToFile();
+		employeeList.getBioinformatician(1).writeAlignmentToFile();
 		
 	}
 
